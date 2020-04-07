2aa3cf84721e5650c8f741974101ced99df4db68
Navid Sheikh <navidsheikh2020@gmail.com>
2020-04-07 19:07:08 +0100

    changed

--- webroot/Project/conn.inc.php
+++ webroot/Project/conn.inc.php
@@ -1,11 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$db = "portafolio";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password,$db);
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("DATABASE_USER");
+$dbpwd = getenv("DATABASE_PASSWORD");
+$dbname = getenv("DATABASE_NAME");
+
+
+
+
+// Creates connection
+$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
